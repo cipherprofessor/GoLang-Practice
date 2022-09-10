5ab3f90e78494d643975cb88d32ba2c82881efdf
Cipher_Professor <mohsinmanzoor1913@gmail.com>
2022-09-10 21:01:07 +0530

    modified:   1st.go

--- 1st.go
+++ 1st.go
@@ -2,7 +2,24 @@ package main
 
 import "fmt"
 
+/* fmt stands for the Format package. This package allows to format basic strings,
+values, or anything and print them or collect user input from the console, or write
+ into a file using a writer or even print customized fancy error messages. This package
+  is all about formatting input and output. */
+
 func main() {
 
 	fmt.Println("Hello Mohsin Manzoor Bhat")
+	fmt.Println("1 + 1 =", 1+1)
 }
+
+/* Go is a procedural programming language. It was developed in 2007 by Robert Griesemer,
+ Rob Pike, and Ken Thompson
+at Google but launched in 2009 as an open-source programming language.
+
+
+
+Golang has been designed in such a way that instead of using threading it uses Goroutine,
+which is similar to threading but consumes very less memory.
+Like threading consumes 1MB whereas Goroutine consumes 2KB of memory,
+ hence at the same time, we can have millions of goroutine triggered. */
